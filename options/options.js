00a1fc11e105c4ebbbd72954d9c307b458bcb9ae
Crowdbotics <team@crowdbotics.com>
2023-04-29 23:07:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "echat-40352",
+  url: "https://echat-40352.botics.co",
+  api: "https://echat-40352.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
